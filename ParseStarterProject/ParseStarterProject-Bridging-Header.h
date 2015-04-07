b2123a5dc83fcd4fbacca0ebeff2a612cc7a03f6
Tommy Gibbons <tommygibbons46@gmail.com>
2015-04-07 12:22:25 -0700

    added the login

--- ParseStarterProject/ParseStarterProject-Bridging-Header.h
+++ ParseStarterProject/ParseStarterProject-Bridging-Header.h
@@ -10,4 +10,9 @@
 // If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
 //#import <ParseFacebookUtils/PFFacebookUtils.h>
 
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
+
+
 #endif
