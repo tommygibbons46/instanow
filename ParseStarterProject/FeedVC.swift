83d211c3ffc982b86d6aa59211f9be78d2fd7dea
Tommy Gibbons <tommygibbons46@gmail.com>
2015-04-07 20:02:24 -0700

    log in updates

--- ParseStarterProject/FeedVC.swift
+++ ParseStarterProject/FeedVC.swift
@@ -1,13 +1,31 @@
 import UIKit
 import Parse
-class FeedVC: UIViewController
+class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate
+
+
+
 {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackground()
+        if PFUser.currentUser() == nil
+        {
+            performSegueWithIdentifier("LoginSegue", sender: self)
+        }
+
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cellID") as UITableViewCell
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 }
 
